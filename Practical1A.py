de97a750d7b652b7c1ae53970ef2ad12abd7c945
Abhishek D Jaiswar <devabhishekjaiswar@gmail.com>
2024-02-04 10:30:08 +0530

    Complete Practical 1A

---
+++ Practical1A.py
@@ -0,0 +1,17 @@
+print("Name: Abhishek Jaiswar")
+print("Roll No. 3")
+n = int(input("Enter the number of inputs:"))
+yin = 0
+for i in range(n):
+    x = float(input("Enter x :"))
+    w = float(input("Enter weight:"))
+    yin = yin + x * w
+
+print("Yin=", yin)
+
+if yin < 0:
+    output = 0
+elif yin > 1:
+    output = 1
+else:
+    output = yin
